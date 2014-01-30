271fb032ce7d08a016e4dd2cba1dadb7c95b8d88
heisenberg <rahul080327@gmail.com>
2014-01-30 23:48:27 +0530

    Functions Implemented

--- bin/transform.py
+++ bin/transform.py
@@ -3,9 +3,11 @@
 import ast, _ast, sys
 
 imports = []
-local = []
-functions = []
+funVars = []
 symTab = []
+
+funMode = False
+func = 0
 
 debug_notification = "**** Medusa Notification ****"
 debug_warning = "**** Medusa Warning ****"
@@ -127,12 +129,17 @@ class MyParser(ast.NodeVisitor):
             i += 1
 
     def visit_Assign(self, stmt_assign):
-        global code
+        global code, funVars
 
         for target in stmt_assign.targets:
-            if symTab.__contains__(target.id) == False:
-                symTab.append(target.id)
-                code += " var"
+            if funMode:
+                if symTab.__contains__(target.id) == False:
+                    symTab.append(target.id)
+                    code += " var"
+            else:
+                if funVars.__contains__(target.id) == False:
+                    funVars.append(target.id)
+                    code += " var"
 
             if isinstance(stmt_assign.value, _ast.Num):
                 value = stmt_assign.value.n
@@ -147,66 +154,66 @@ class MyParser(ast.NodeVisitor):
 
             code += " " + target.id + " = " + str(value) + ";"
 
-    def visit_If(self, stmt_If):
+    def visit_If(self, stmt_if):
         global code
 
         code += "if("
-        if hasattr(stmt_If.test, 'left'):
-            varType = str(type(stmt_If.test.left))[13:-2]
+        if hasattr(stmt_if.test, 'left'):
+            varType = str(type(stmt_if.test.left))[13:-2]
             if varType == "Name":
-                if stmt_If.test.left.id == 'True':
+                if stmt_if.test.left.id == 'True':
                     code += "true"
-                elif stmt_If.test.left.id == 'False':
+                elif stmt_if.test.left.id == 'False':
                     code += "false"
                 else:
-                    code += stmt_If.test.left.id
+                    code += stmt_if.test.left.id
             elif varType == "Str":
-                code += stmt_If.test.left.s
+                code += stmt_if.test.left.s
             elif varType == "Num":
-                code += str(stmt_If.test.left.n)
+                code += str(stmt_if.test.left.n)
             elif varType == "BinOp":
-                code += self.parseExp(stmt_If.test.left)
+                code += self.parseExp(stmt_if.test.left)
             else:
                 print debug_warning
                 print "Type not recognized => ", varType
-        elif str(type(stmt_If.test))[13:-2] == "Name":
-            if stmt_If.test.id == "True":
+        elif str(type(stmt_if.test))[13:-2] == "Name":
+            if stmt_if.test.id == "True":
                 code += "true"
-            elif stmt_If.test.id == "False":
+            elif stmt_if.test.id == "False":
                 code += "false"
             else:
-                print type(stmt_If.test.id)
+                print type(stmt_if.test.id)
 
-        if hasattr(stmt_If.test, 'ops'):
-            code += operators[str(type(stmt_If.test.ops[0]))[8:-2]]
+        if hasattr(stmt_if.test, 'ops'):
+            code += operators[str(type(stmt_if.test.ops[0]))[8:-2]]
 
-        if hasattr(stmt_If.test, 'comparators'):
-            varType = str(type(stmt_If.test.comparators[0]))[13:-2]
+        if hasattr(stmt_if.test, 'comparators'):
+            varType = str(type(stmt_if.test.comparators[0]))[13:-2]
             if varType == "Name":
-                if stmt_If.test.comparators[0].id == 'True':
+                if stmt_if.test.comparators[0].id == 'True':
                     code += "true"
-                elif stmt_If.test.comparators[0].id == 'False':
+                elif stmt_if.test.comparators[0].id == 'False':
                     code += "false"
                 else:
-                    code += stmt_If.test.comparators[0].id
+                    code += stmt_if.test.comparators[0].id
             elif varType == "Str":
-                code += stmt_If.test.comparators[0].s
+                code += stmt_if.test.comparators[0].s
             elif varType == "Num":
-                code += str(stmt_If.test.comparators[0].n)
+                code += str(stmt_if.test.comparators[0].n)
             elif varType == "BinOp":
-                code += self.parseExp(stmt_If.test.comparators[0])
+                code += self.parseExp(stmt_if.test.comparators[0])
             else:
                 print debug_warning
                 print "Type not recognized => ", varType
 
         code += ") {"
-        for node in stmt_If.body:
+        for node in stmt_if.body:
             self.visit(node)
 
         code += "}"
-        if len(stmt_If.orelse) > 0:
+        if len(stmt_if.orelse) > 0:
             code += " else {"
-            for node in stmt_If.orelse:
+            for node in stmt_if.orelse:
                 self.visit(node)
             code += "}"
 
@@ -228,56 +235,115 @@ class MyParser(ast.NodeVisitor):
             for node in stmt_For.orelse:
                 self.visit(node)
 
-    def visit_While(self, stmt_While):
+    def visit_While(self, stmt_while):
         global code
 
         code += " while ("
-        varType = str(type(stmt_While.test.left))[13:-2]
+        varType = str(type(stmt_while.test.left))[13:-2]
 
         if varType == "Name":
-            if stmt_While.test.left.id == 'True':
+            if stmt_while.test.left.id == 'True':
                 code += "true"
-            elif stmt_While.test.left.id == 'False':
+            elif stmt_while.test.left.id == 'False':
                 code += "false"
             else:
-                code += stmt_While.test.left.id
+                code += stmt_while.test.left.id
         elif varType == "Str":
-           code += stmt_While.test.left.s
+           code += stmt_while.test.left.s
         elif varType == "Num":
-            code += str(stmt_While.test.left.n)
+            code += str(stmt_while.test.left.n)
         else:
             print debug_warning
             print "Type not recognized => ", varType
 
-        code += operators[str(type(stmt_While.test.ops[0]))[8:-2]]
-        varType = str(type(stmt_While.test.comparators[0]))[13:-2]
+        code += operators[str(type(stmt_while.test.ops[0]))[8:-2]]
+        varType = str(type(stmt_while.test.comparators[0]))[13:-2]
 
         if varType == "Name":
-            if stmt_While.test.comparators[0].id == 'True':
+            if stmt_while.test.comparators[0].id == 'True':
                 code += "true"
-            elif stmt_While.test.comparators[0].id == 'False':
+            elif stmt_while.test.comparators[0].id == 'False':
                 code += "false"
             else:
-                code += stmt_While.test.comparators[0].id
+                code += stmt_while.test.comparators[0].id
         elif varType == "Str":
-            code += stmt_While.test.comparators[0].s
+            code += stmt_while.test.comparators[0].s
         elif varType == "Num":
-            code += str(stmt_While.test.comparators[0].n)
+            code += str(stmt_while.test.comparators[0].n)
         else:
             print debug_warning
             print "Type not recognized => ", varType
 
         code += ") "
         code += " {"
-        for node in stmt_While.body:
+        for node in stmt_while.body:
             self.visit(node)
 
-        code += stmt_While.test.left.id
+        code += stmt_while.test.left.id
         code += "++; "
         code += "}"
 
     def visit_FunctionDef(self, stmt_function):
-        print stmt_function.name
+        global code, funVars
+
+        temp = code
+        code = ""
+        funMode = True
+
+        alen = len(stmt_function.args.args)
+        code = stmt_function.name + "("
+
+        if alen == 0:
+            code += ") {"
+        else:
+            i = 0
+            while i < alen:
+                code += stmt_function.args.args[i].id
+                funVars.append(stmt_function.args.args[i].id)
+
+                if (i + 1) < alen:
+                    code += ", "
+                else:
+                    code += ") {"
+                i += 1
+
+        for node in stmt_function.body:
+            self.visit(node)
+        funMode = False
+        code += " } "
+        funVars = []
+
+        code = code + temp
+
+    def visit_Call(self, stmt_call):
+        global code
+
+        code += " " + stmt_call.func.id + "("
+        alen = len(stmt_call.args)
+
+        if alen == 0:
+            code += ");"
+        else:
+            i = 0
+            while i < alen:
+                if isinstance(stmt_call.args[i], _ast.Name):
+                    p = stmt_call.args[i].id
+                elif isinstance(stmt_call.args[i], _ast.Num):
+                    p = stmt_call.args[i].n
+                elif isinstance(stmt_call.args[i], _ast.Str):
+                    p = "'" + stmt_call.args[i].s + "'"
+                elif isinstance(stmt_call.args[i], _ast.List):
+                    p = self.parseList(stmt_call.args[i].elts)
+                elif isinstance(stmt_call.args[i], _ast.BinOp):
+                    p = self.parseExp(stmt_call.args[i])
+
+                code += str(p)
+
+                if (i + 1) < alen:
+                    code += ", "
+                else:
+                    code += ");"
+                i += 1
 
 MyParser().parse(open(sys.argv[1]).read())
 
@@ -285,9 +351,6 @@ code += " }"
 
 for imp in imports:
     code = "import '" + imp + "'; " + code
-
-for func in functions:
-    code = func + code
 
 outFile.write(code)
 outFile.close()
