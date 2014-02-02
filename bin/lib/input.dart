e097a9ace6eba932d6ca46c422942b0e0eb4989f
heisenberg <rahul080327@gmail.com>
2014-02-02 09:38:03 +0530

    STDIN Input Fixed

--- bin/lib/input.dart
+++ bin/lib/input.dart
@@ -2,8 +2,24 @@ library input;
 
 import 'dart:io';
 
-String input([message]) {
+String raw_input([message]) {
     if (message != null)
         print(message);
+
     return stdin.readLineSync(encoding: SYSTEM_ENCODING);
+}
+
+num input([message]) {
+    if (message != null)
+        print(message);
+
+    num value;
+
+    try {
+        value = num.parse(stdin.readLineSync(encoding: SYSTEM_ENCODING));
+        return value;
+    } catch (ex) {
+        print("Fatal Error: Non numeric characters in input");
+        exit(1);
+    }
 }
