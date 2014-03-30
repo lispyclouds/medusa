f6d0aff00793323aa48786a62808643ffb2b97f9
heisenberg <rahul080327@gmail.com>
2014-03-30 12:16:01 +0530

    Added help and compile only commad line options

--- src/exec.cpp
+++ src/exec.cpp
@@ -1,12 +1,15 @@
 #include <exec.h>
 
-void Exec::run(QString code, QString fileName) {
+void Exec::run(QString code, QString fileName, bool cStop) {
     QFile out(fileName);
     QString command = "dart " + fileName;
 
     out.open(QIODevice::WriteOnly | QIODevice::Text);
     out.write(code.toStdString().c_str());
     out.close();
+
+    if (cStop)
+        return;
 
     system(command.toStdString().c_str());
     out.remove();
--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,54 @@
 #include <QFileInfo>
 #include <QtCore/QCoreApplication>
+#include <iostream>
 #include <cache.h>
 #include <transform.h>
 #include <exec.h>
+
+using namespace std;
+
+string helpText = "\nMedusa 1.0 beta\nCopyright (c) 2013-2104, Rahul De, Apoorv Agarwal and Aakash Giri, VIT University\n\
+Licensed under BSD 3-Clause\n\nUsage: medusa [options] file\n\nOptions:\n\
+--help\tDispaly this information\n-c\tCompile to Dart only\n";
 
 int main(int argc, char **argv) {
     QCoreApplication app(argc, argv);
     Cache cache;
     Transform transformer;
     Exec exec;
+    QStringList args = app.arguments();
+    bool cStop = false;
+    QString path, name, code, art;
+    QFile artFile("art.txt");
 
-    QFileInfo pyFile(app.arguments().at(1));
-    QString path = pyFile.absoluteFilePath(), name = pyFile.completeBaseName() + ".dart", code;
+    if (app.arguments().size() == 1) {
+        artFile.open(QIODevice::ReadOnly | QIODevice::Text);
+        art = artFile.readAll();
+        artFile.close();
+
+        cout << helpText << art.toStdString();
+        return 0;
+    }
+
+    foreach(QString arg, app.arguments()) {
+        if (arg == "--help") {
+            artFile.open(QIODevice::ReadOnly | QIODevice::Text);
+            art = artFile.readAll();
+            artFile.close();
+
+            cout << helpText << art.toStdString();
+            return 0;
+        } else if (arg == "-c")
+            cStop = true;
+        else {
+            QFileInfo pyFile(arg);
+            path = pyFile.absoluteFilePath();
+            name = pyFile.completeBaseName() + ".dart";
+        }
+    }
 
     if (cache.isCached(path, code) || transformer.transform(path, code))
-        exec.run(code, name);
+        exec.run(code, name, cStop);
 
     return 0;
 }
